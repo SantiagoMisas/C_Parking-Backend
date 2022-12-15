0c81b3ac3407393205fdb6827b789effe14b58b7
SantiagoMisas <santiagomisaspulgarin@gmail.com>
2022-12-15 15:38:45 -0500

    Se editan los models refactorizados

--- models/modelProducto.js
+++ models/modelProducto.js
@@ -2,15 +2,10 @@ import { DataTypes } from "sequelize";
 import dataBase from "../database/connectDatabase";
 
 const Producto=dataBase.define('producto', {
-    Codigo_De_Producto: {type: DataTypes.STRING},
-    Nombre_De_Producto: {type: DataTypes.STRING},
-    Numero_De_Factura: {type: DataTypes.STRING},
-    Fecha_De_Entrada: {type: DataTypes.DATE},
-    Fecha_De_Salida: {type: DataTypes.DATE},
-    Seccion: {type: DataTypes.STRING},
-    Valor: {type: DataTypes.FLOAT},
-    Descripcion: {type: DataTypes.STRING},
-    Cantidad: {type: DataTypes.BIGINT},
+    nombre: {type: DataTypes.STRING},
+    precio: {type: DataTypes.DOUBLE},
+    inventario: {type: DataTypes.INT},
+    descripcion: {type: DataTypes.STRING},
 })
 
 export default Producto
--- models/modelVehiculo.js
+++ models/modelVehiculo.js
@@ -2,13 +2,12 @@ import { DataTypes } from "sequelize";
 import dataBase from "../database/connectDatabase";
 
 const Vehiculo=dataBase.define('vehiculo', {
-    Placa_Vehiculo: {type: DataTypes.STRING},
-    Cedula: {type: DataTypes.STRING},
-    Tipo_De_Vehiculo: {type: DataTypes.STRING},
-    Hora_De_Ingreso: {type: DataTypes.TIME},
-    Fecha_De_Ingreso: {type: DataTypes.DATE},
-    Hora_De_Salida: {type: DataTypes.TIME},
-    Fecha_De_Salida: {type: DataTypes.DATE},
+    placa: {type: DataTypes.STRING},
+    fecha_ingreso: {type: DataTypes.DATE},
+    hora_ingreso: {type: DataTypes.TIME},
+    hora_salida: {type: DataTypes.TIME},
+    horas: {type: DataTypes.BIGINT},
+
 })
 
 export default Vehiculo
